cc23f23bcbb0ea06130ad6ca53b0c68f949dac0c
danpatrickoneill <dan47573@gmail.com>
2019-10-14 17:16:16 -0400

    Removed unnecessary base class

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('../doubly_linked_list')
 
 
-class Stack(DoublyLinkedList):
+class Stack():
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
