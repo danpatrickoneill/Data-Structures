7bb499e67daf774eda48e4a4dc5db5eae82b10c3
danpatrickoneill <dan47573@gmail.com>
2019-10-14 15:51:14 -0400

    Refactored to use dll length attr; all tests pass

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -6,20 +6,19 @@ sys.path.append('../doubly_linked_list')
 class Queue(DoublyLinkedList):
     def __init__(self, node=None):
         super().__init__(node)
-        self.size = self.length
+        # self.size = self.length
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
-        self.storage = None
 
     def enqueue(self, value):
         self.add_to_tail(value)
-        self.size += 1
+        # self.size += 1
 
     def dequeue(self):
-        if self.size == 0:
+        if self.length == 0:
             return None
-        self.size -= 1
+        # self.size -= 1
         return self.remove_from_head()
 
     def len(self):
-        return self.size
+        return self.length
