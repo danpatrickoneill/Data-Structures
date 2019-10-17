ff45316fa577eaa7060b6f2ea5771c774f93375d
danpatrickoneill <dan47573@gmail.com>
2019-10-17 15:55:11 -0400

    Set up LRU cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +9,12 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.size = 0
+        self.cache = DoublyLinkedList()
+        self.storage = {}
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,9 +23,13 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
-    def get(self, key):
-        pass
 
+    def get(self, key):
+        if key not in self.storage:
+            return None
+        node = self.storage[key]
+        self.cache.move_to_front(node)
+        return node.value[1]
     """
     Adds the given key-value pair to the cache. The newly-
     added pair should be considered the most-recently used
@@ -29,5 +40,20 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        if key in self.storage:
+            # print("Key found:", key, self.storage[key], value)
+            node = self.storage[key]
+            node.value = (key, value)
+            self.cache.move_to_front(node)
+            return
+        else:
+            if self.size == self.limit:
+                node = self.cache.tail
+                key_to_remove = node.value[0]
+                self.cache.remove_from_tail()
+                del self.storage[key_to_remove]
+            self.cache.add_to_head((key, value))
+            self.storage[key] = self.cache.head
+            self.size += 1
